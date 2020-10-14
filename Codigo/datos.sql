4e2d7ed6be3a4d208cf5c726595f632757f78cf9
tridoxx <julianhoyos91@gmail.com>
2020-10-14 17:26:51 -0500

    Add files via upload

---
+++ Codigo/datos.sql
@@ -0,0 +1,80 @@
+-- MySQL dump 10.17  Distrib 10.3.24-MariaDB, for debian-linux-gnueabihf (armv8l)
+--
+-- Host: localhost    Database: casino
+-- ------------------------------------------------------
+-- Server version	10.3.24-MariaDB-2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tipo_transaccion`
+--
+
+DROP TABLE IF EXISTS `tipo_transaccion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tipo_transaccion` (
+  `id_tipotransaccion` int(11) NOT NULL,
+  `nombre` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id_tipotransaccion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipo_transaccion`
+--
+
+LOCK TABLES `tipo_transaccion` WRITE;
+/*!40000 ALTER TABLE `tipo_transaccion` DISABLE KEYS */;
+INSERT INTO `tipo_transaccion` VALUES (0,'prueba'),(1,'recarga'),(2,'apuesta');
+/*!40000 ALTER TABLE `tipo_transaccion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transacciones`
+--
+
+DROP TABLE IF EXISTS `transacciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transacciones` (
+  `id_transaccion` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha_transaccion` timestamp NOT NULL DEFAULT current_timestamp(),
+  `total_transaccion` double NOT NULL,
+  `detalle_transacion` double NOT NULL DEFAULT 0,
+  `tipo_transacion` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id_transaccion`),
+  KEY `transacciones_FK` (`tipo_transacion`),
+  CONSTRAINT `transacciones_FK` FOREIGN KEY (`tipo_transacion`) REFERENCES `tipo_transaccion` (`id_tipotransaccion`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transacciones`
+--
+
+LOCK TABLES `transacciones` WRITE;
+/*!40000 ALTER TABLE `transacciones` DISABLE KEYS */;
+INSERT INTO `transacciones` VALUES (1,'2020-10-12 01:28:06',0,0,0);
+/*!40000 ALTER TABLE `transacciones` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-10-11 20:28:10
